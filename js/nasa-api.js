4f6853852434999b537ca2bf40c19cb92d83f045
Rebecca Kennedy <rebecca.kennedy2@pcc.edu>
2017-06-02 01:40:44 -0700

    Fixed Double Click Bug
    I fixed the bug in nasa-api.js that caused the user to have to click the
    list item twice to view the planet information. Now the user should only
    have to click once.

--- js/nasa-api.js
+++ js/nasa-api.js
@@ -1,5 +1,5 @@
 /* File Name: nasa-api.js */
-/* Date: 05/30/17 */
+/* Date: 06/02/17 */
 /* Programmer: Rebecca Kennedy */
 
 /* eslint-env jquery */
@@ -7,66 +7,91 @@
 /* global document, $ */
 
 $(document).ready(function() {
-
-var beginUrl = "https://api.nasa.gov/planetary/apod?api_key=KDngDjbv8NilLaR3FEFcHvC72IMAtILiF3H1X4Cy&date=";
-
-var date;
-var planet;    
-    
-    /* When a list item is selected by the user, a date should be stored in the date variable and the classname is stored in the planet variable.*/
-    $("#planLinks li").click(function() {
-        if ($(this).hasClass("mercury")) {
-                 date = "2004-09-12";
-         planet = ".mercury";
-        } else if ($(this).hasClass("venus")) {
-            date = "2004-05-16";
-            planet = ".venus";
-        } else if ($(this).hasClass("earth")) {
-            date = "2005-01-02";
-            planet = ".earth";
-        } else if ($(this).hasClass("mars")) {
-            date = "2001-07-18";
-            planet = ".mars";
-        } else if ($(this).hasClass("jupiter")) {
-            date = "2006-05-05";
-            planet = ".jupiter";
-        } else if ($(this).hasClass("saturn")) {
-            date = "2003-08-17";
-            planet = ".saturn";
-        } else if ($(this).hasClass("uranus")) {
-            date = "2001-08-26";
-            planet = ".uranus";
-        } else if ($(this).hasClass("neptune")) {
-            date = "1998-02-21";
-            planet = ".neptune";
-        }
-        /* The URL is concatinated together. */
-        var url = beginUrl + date;
-
-        /* The url and planet variables are passed in and information is displayed on the HTML screen. */
-        $(planet).click(function() {
-            $.ajax({ 
-                url: url,
-                success: function(result){
-                if("planCopyright" in result) {
-                  $("#planCopyright").text("Image Credits: " + result.copyright);
-                }
-                else {
-                  $("#planCopyright").text("Image Credits: " + "Public Domain");
-                }
-  
-                if(result.media_type == "video") {
-                  $("#planImg").css("display", "none"); 
-                  $("#apod_vid_id").attr("src", result.url);
-                }
-                else {
-                  $("#apod_vid_id").css("display", "none"); 
-                  $("#planImg").attr("src", result.url);
-                }  
-                $("#planText").text(result.explanation);
-                $("#planName").text(result.title);
-                }
-            });
+    var urlBefore = "https://api.nasa.gov/planetary/apod?api_key=KDngDjbv8NilLaR3FEFcHvC72IMAtILiF3H1X4Cy&date=";
+    $(".mercury").click(function() {
+        $.ajax({ 
+            url: urlBefore + "2004-09-12",
+            success: results(function() {
+            })
         });
     });
+    
+    $(".venus").click(function() {
+        $.ajax({ 
+            url: urlBefore + "2004-05-16",
+            success: results(function() {
+            })
+        });
+    });
+    
+    $(".earth").click(function() {
+        $.ajax({ 
+            url: urlBefore + "2005-01-02",
+            success: results(function() {
+            })
+        });
+    });
+    
+    $(".mars").click(function() {
+        $.ajax({ 
+            url: urlBefore + "2001-07-18",
+            success: results(function() {
+            })
+        });
+    });
+    
+    $(".jupiter").click(function() {
+        $.ajax({ 
+            url: urlBefore + "2006-05-05",
+            success: results(function() {
+            })
+        });
+    });
+    
+    $(".saturn").click(function() {
+        $.ajax({ 
+            url: urlBefore + "2003-08-17",
+            success: results(function() {
+            })
+        });
+    });
+    
+    $(".uranus").click(function() {
+        $.ajax({ 
+            url: urlBefore + "2001-08-26",
+            success: results(function() {
+            })
+        });
+    });
+ 
+    $(".neptune").click(function() {
+        $.ajax({ 
+            url: urlBefore + "1998-02-21",
+            success: results(function() {
+            })
+        });
+    });
+    
+    function results(func) {
+        return function(data) {
+            if("planCopyright" in data) {
+                $("#planCopyright").text("Image Credits: " + data.copyright);
+            }
+            else {
+                func(data);
+                $("#planCopyright").text("Image Credits: " + "Public Domain");
+            }
+  
+            if(data.media_type == "video") {
+                $("#planImg").css("display", "none"); 
+                $("#apod_vid_id").attr("src", data.url);
+            }
+            else {
+                $("#apod_vid_id").css("display", "none"); 
+                $("#planImg").attr("src", data.url);
+            }  
+            $("#planText").text(data.explanation);
+            $("#planName").text(data.title);
+         };
+    }
 });
